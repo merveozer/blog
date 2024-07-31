e6dea4eb350ad3e67de2ea96dc9541e88f4e486b
merveozer <ozeermerve@gmail.com>
2024-07-31 15:21:26 +0300

    BLAP-10111: Created JWT Token Provider and Role Entity

--- src/main/java/com/app/blog/security/JwtUserDetails.java
+++ src/main/java/com/app/blog/security/JwtUserDetails.java
@@ -1,4 +1,94 @@
 package com.app.blog.security;
 
-public class JwtUserDetails {
+
+import com.app.blog.persistence.entity.RoleEntity;
+import com.app.blog.persistence.entity.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class JwtUserDetails implements UserDetails {
+    public Long id;
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    private JwtUserDetails(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public static JwtUserDetails create(UserEntity user) {
+        Set<RoleEntity> roles = user.getRole();
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        for(RoleEntity role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+
+
+        authorities.add(new SimpleGrantedAuthority("user"));
+
+        return new JwtUserDetails(user.getUserId(), user.getUserName(), user.getPassword(), authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
